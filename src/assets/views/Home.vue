d5d54f609c7f587c1358cf2cd7e11d43d11375eb
ask1666 <geeky_assb@live.no>
2020-07-05 21:47:30 +0200

    can display dashboards

--- src/assets/views/Home.vue
+++ src/assets/views/Home.vue
@@ -4,12 +4,52 @@
         class="p-5 text-2xl text-center text-white">
         Home
         </h1>
+        <h1 v-if="errorMessage">
+            {{errorMessage}}
+        </h1>
+        <div v-if="(this.dashboards === undefined || this.dashboards.length === 0)">
+            <h1 class="p-5 text-xl text-center text-red-700">
+                You have no dashboards. 
+            </h1>
+            <!-- TODO create new createDashboardComponent -->
+        </div>
+        <ul v-else-if="!(this.dashboards === undefined || this.dashboards.length === 0)">
+            <li class="p-3" v-for="dashboard in dashboards" v-bind:key="dashboard.title">
+                <p class="p-2 text-xl text-center text-black bg-blue-500 rounded">
+                    {{dashboard.title}}
+                </p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import Axios from 'axios';
+
+Axios.defaults.withCredentials = true;
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    
+    data() {
+        return {
+            dashboards: [],
+            errorMessage: null
+        };
+    },
+    created() {
+        Axios.get("http://localhost:4000/api/users/show_user")
+            .then (res => {
+                this.dashboards = res.data.data.dashboards;
+                //console.log(this.dashboards)
+            })
+            .catch (err => {
+                console.log(err);
+                this.errorMessage = "You are not logged in" 
+            })
+    }, methods: {
+        
+    }
 }
 </script>
 
--- src/assets/views/Login.vue
+++ src/assets/views/Login.vue
@@ -5,21 +5,21 @@
         Login
         </h1>
         <div class=" pt-5 w-full w-full flex justify-center ">
-            <form class="bg-blue-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form class="bg-blue-500 shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit="login">
                 <div class="mb-4">
                 <label class="block text-black text-sm font-bold mb-2" for="email">
                     Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Email">
+                <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Email">
                 </div>
                 <div class="mb-6">
                 <label class="block text-black text-sm font-bold mb-2" for="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" placeholder="Password" >
+                <input v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="Password" >
                 </div>
                 <div class="flex items-center justify-between">
-                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Sign In
                 </button>
                 <a class="inline-block align-baseline font-bold text-sm text-gray-700 hover:text-blue-900" href="#">
@@ -33,8 +33,33 @@
 </template>
 
 <script>
+import Axios from 'axios';
+
+Axios.defaults.withCredentials = true;
 export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        login: function (e) {
+            e.preventDefault()
+            
+            Axios.post('http://localhost:4000/api/users/sign_in', {
+                email: this.email,
+                password: this.password
+            })
+            .then (res => {
+                console.log(res);
+            })
+            .catch (err => {
+                console.log(err);
+            })
+        }
+    }
 }
 </script>
 
